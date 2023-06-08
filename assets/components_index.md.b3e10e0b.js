a7f5ef0e544e086183e2c6b31b6db51f5fe1c7b8
quwensong <71798319+quwensong@users.noreply.github.com>
2023-06-08 03:36:33 +0000

    Deploying to gh-pages from @ Aim-Design/aim-plus@2f4abcbcfabc62e811376e5c2bd11c6eb933549f 🚀

---
+++ assets/components_index.md.b3e10e0b.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as n}from"./chunks/framework.cec2b577.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"components/index.md","lastUpdated":1686195314000}'),a={name:"components/index.md"};function o(s,r,c,d,p,i){return n(),t("div")}const f=e(a,[["render",o]]);export{m as __pageData,f as default};
