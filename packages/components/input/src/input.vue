1905f4152afffe8b25c0e9b7589d132ba5a74bfd
quwensong <1607186824@qq.com>
2023-06-09 15:55:46 +0800

    feat: 增加button,密码输入框

--- packages/components/input/src/input.vue
+++ packages/components/input/src/input.vue
@@ -9,14 +9,26 @@
       <aim-icon v-if="prefixIcon" :name="prefixIcon" />
     </span>
     <input
+      ref="input"
       :class="[n('inner'), styleClass]"
       :disabled="props.disabled"
       :placeholder="props.placeholder"
       v-model="value"
       v-bind="attrs"
     />
-    <span @click.stop="clearValue" v-if="props.clearable" :class="n('suffix')">
-      <aim-icon v-show="showClear && value" name="x-circle-fill" />
+    <span v-if="props.clearable || props.showPassword" :class="n('suffix')">
+      <template v-if="showPassword">
+        <aim-icon
+          :name="passwordView ? 'eye' : 'eye-slash'"
+          @click="handleViewPassword"
+        />
+      </template>
+      <aim-icon
+        v-else
+        v-show="showClear && value"
+        name="x-circle"
+        @click.stop="clearValue"
+      />
     </span>
   </div>
 </template>
@@ -57,6 +69,27 @@ function handleMouseEnter() {
 function handleMouseLeave() {
   showClear.value = !!value.value;
 }
+
+//显示隐藏密码框 showPassword
+const input = ref();
+const passwordView = ref(true); //密码是否可见
+
+Promise.resolve().then(() => {
+  if (!props.showPassword) {
+    input.value.type = 'password';
+  }
+});
+
+// 点击眼睛
+const handleViewPassword = () => {
+  passwordView.value = !passwordView.value;
+
+  if (input.value.type === 'password') {
+    input.value.type = attrs.type || 'text';
+    return;
+  }
+  input.value.type = 'password';
+};
 
 //根据props更改类名
 const styleClass = computed(() => {
